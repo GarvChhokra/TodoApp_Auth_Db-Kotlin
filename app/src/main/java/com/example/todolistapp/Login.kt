626fe03189c93de640637b9b55ea80e4b971be13
GarvChhokra <garvchhokra@gmail.com>
2023-12-30 04:43:19 -0500

    register using email and password in firebase. did some ui changes

--- app/src/main/java/com/example/todolistapp/Login.kt
+++ app/src/main/java/com/example/todolistapp/Login.kt
@@ -1,11 +1,24 @@
 package com.example.todolistapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolistapp.databinding.ActivityLoginBinding
+import java.util.zip.Inflater
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnRegister.setOnClickListener {
+                val intent = Intent(this@Login, Register::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/todolistapp/Register.kt
+++ app/src/main/java/com/example/todolistapp/Register.kt
@@ -1,13 +1,65 @@
 package com.example.todolistapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.todolistapp.databinding.ActivityRegisterBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class Register : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        binding.apply {
+            btnRegister.setOnClickListener {
+                if(editTextEmail.text.toString().isEmpty()){
+                    Toast.makeText(this@Register, "Enter your Email", Toast.LENGTH_SHORT).show()
+                }
+                if(editTextPassword.text.toString().isEmpty()){
+                    Toast.makeText(this@Register, "Enter your Password", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                auth.createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
+                    .addOnCompleteListener(this@Register) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this@Register, "Account Created", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this@Register, Login::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+                }
+            }
+//            btnGoogleRegister.setOnClickListener {
+//                signInRequest = BeginSignInRequest.builder()
+//                    .setGoogleIdTokenRequestOptions(
+//                        BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+//                            .setSupported(true)
+//                            // Your server's client ID, not your Android client ID.
+//                            .setServerClientId(getString(R.string.your_web_client_id))
+//                            // Only show accounts previously used to sign in.
+//                            .setFilterByAuthorizedAccounts(true)
+//                            .build())
+//                    .build()
+//            }
+        }
     }
 }
